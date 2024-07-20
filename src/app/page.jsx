9c431cbfbb58b8e2f50b550fe1251ac6074dc4bb
kauanbrtech <kauandasilva@brtech.dev>
2024-07-20 20:21:27 -0300

    "Remove admin layout and page, update home page with notification feature"

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,39 @@
 "use client"
-import { Suspense } from "react"
+import { Suspense, useEffect } from "react"
 import Loading from "./loading"
 import TopSection from "./home/TopSection"
 import MiddleSection from "./home/MiddleSection"
 
 
 const Home = () => {
+
+  useEffect(() => {
+    const requestPermissionAndNotify = () => {
+      window.Notification.requestPermission(permission => {
+        if (permission === 'granted') {
+          new window.Notification("Vishi Delivery", {
+            body: "Seja Bem-Vindo",
+            icon: "/img/logo.svg"
+          })
+        }
+      })
+    }
+
+    requestPermissionAndNotify(); // Pedir permissão e notificar na primeira vez
+
+    const intervalId = setInterval(() => {
+      new window.Notification("Vishi Delivery", {
+        body: "Aqui está sua notificação periódica",
+        icon: "/img/logo.svg"
+      })
+    }, 60000); // 60000 ms = 1 minuto
+
+    // Cleanup function
+    return () => clearInterval(intervalId);
+  }, [])
+
+
+
   return (
     <Suspense fallback={<Loading />}>
       <TopSection />
